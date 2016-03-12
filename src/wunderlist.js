12d63efe3ae0f01e936d3ad1ac7ed94b2ad10070
note109 <note109+github@gmail.com>
2016-03-12 23:21:25 +0900

    add comment

--- src/wunderlist.js
+++ src/wunderlist.js
@@ -34,6 +34,7 @@ let Wunderlist = class {
     return result;
   }
 
+  // TODO: redisも使う
   static fetchList() {
     return new Promise((resolve, reject) => {
       wunderlistsdk.http.lists.all().done((lists) => {
